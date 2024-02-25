6c6618b942af8c8a7fbeab7db303ad0e8be91581
ILLalev22 <132440245+ILLalev22@users.noreply.github.com>
2024-02-26 00:33:08 +0200

    Comments add
    I added comments to the main.cpp

--- Project3/Main.cpp
+++ Project3/Main.cpp
@@ -1,3 +1,5 @@
+//PSSI_Studio projects
+
 #include "GameEngine.h"
 
 int main() {
@@ -5,3 +7,9 @@ int main() {
     engine.run();
     return 0;
 }
+
+//GameEngine.cpp - The main game logic, it runs the physics the UI and over all every element!
+//Button.cpp - This is the most important button which is the close button! It handles it's events!
+//GameEngine.h- This is for storing the variables for the game Engine so the files doesn't become too long!
+//Button.h - Used for the same purpose!
+//SFML - Mainly used!
