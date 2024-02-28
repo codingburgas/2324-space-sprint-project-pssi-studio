67e0819c29cbbf8f0e54287df7c2e802d96cfcd8
Ivailo <ILLalev22@codingburgas.bg>
2024-02-28 13:05:37 +0200

    hitbox fix

--- Project3/GameEngine.cpp
+++ Project3/GameEngine.cpp
@@ -562,7 +562,7 @@ void GameEngine::updateMeteors(float deltaTime) {
 
 void GameEngine::checkCollisions() {
     // Used for detecting the colissions with the meteors! Also the Shrink factor makes it easier for the player!
-    float shrinkFactor = 0.1f;
+    float shrinkFactor = 10f;
     for (auto& meteor : meteors) {
         sf::FloatRect reducedSpaceShipBounds = spaceShip.getGlobalBounds();
         reducedSpaceShipBounds.left += reducedSpaceShipBounds.width * (1 - shrinkFactor) / 2;
